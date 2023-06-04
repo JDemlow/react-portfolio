64e83ce282efd781c9aa6965269e4646dafeff68
James Demlow <jdemlow@gmail.com>
2023-06-03 22:07:05 -0600

    fixing tailwind media queries

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
         action="https://getform.io/f/b97b661d-5b4b-40e0-9158-3836b39f4565"
         className="flex flex-col max-w-[600px] w-full"
       >
-        <div className="pb-8">
+        <div className="p-4 md:pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-[--accent-bg] text-gray-300">
             Contact
           </p>
