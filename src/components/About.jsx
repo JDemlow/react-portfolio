6f11df3dedc5e219ade6184daf6d86bce9503b3f
James Demlow <jdemlow@gmail.com>
2023-06-03 22:49:40 -0600

    fixing tailwind media queries

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,7 +30,7 @@ const About = () => {
         </div>
         <div className="flex justify-center items-center">
           <img
-            className="w-32 md:w-60 mx-auto my-4 py-8 hover:scale-110 duration-500 mb-12"
+            className="w-32 md:w-60 mx-auto my-4 py-8 hover:scale-110 duration-500 mb-20"
             src={restaurantImage}
             alt="picture of James"
           />
