878497cdc68aa80ce254ae3680ca1e9c9cd2a492
James Demlow <jdemlow@gmail.com>
2023-06-04 20:08:11 -0600

    fixing tailwind media queries - padding changes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -10,7 +10,7 @@ const Work = () => {
       className="w-full md:h-screen text-gray-300 bg-[--primary-bg]"
     >
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-        <div>
+        <div className="py-20 md:py-0">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-[--accent-bg]">
             Work
           </p>
