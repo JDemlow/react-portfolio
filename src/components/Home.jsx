0324aa4e6559ff92ab67b1ab452deb7bf5d81f9a
James Demlow <jdemlow@gmail.com>
2023-06-04 20:53:41 -0600

    adjusted color palette

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,14 +11,14 @@ const Home = () => {
     <div name="home" className="w-full h-screen bg-[--primary-bg]">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-[--primary-text]">Hi, my name is</p>
+        <p className="text-white">Hi, my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
           James Demlow
         </h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#e7ecef]">
           Full Stack Developer
         </h2>
-        <p className="text-[#8892b0] py-4 max-w-[700px]">
+        <p className="text-[#ccd6f6] py-4 max-w-[700px]">
           I'm a software engineer with years of experience building responsive
           and accessible websites and web applications. I'm interested in
           creating clean/well-tested code, using interpersonal abilities, and
